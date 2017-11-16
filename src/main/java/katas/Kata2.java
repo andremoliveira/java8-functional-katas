91621a96f7275a31ae4878f2c0465f9a00ab4e2f
André Machado Oliveira <amachado@fitec.org.br>
2017-11-16 11:34:57 -0200

    Resolution of Kata 2.

--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -1,22 +1,29 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import model.Movie;
 import util.DataUtil;
-
-import java.util.List;
-import java.util.Map;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
     DataSource: DataUtil.getMovies()
     Output: List of Integers
 */
-public class Kata2 {
-    public static List<Integer> execute() {
+public class Kata2
+{
+    public static List<Integer> execute()
+    {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(1, 2, 3);
+        return movies.stream().filter(movie -> movie.getRating().equals(5.0)).map(movie -> movie.getId()).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args)
+    {
+
+        System.out.println(execute());
+
     }
 }
