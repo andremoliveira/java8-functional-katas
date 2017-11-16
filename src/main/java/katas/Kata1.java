13a994e1beca270b22e93abe54f5723cecd3cb79
André Machado Oliveira <amachado@fitec.org.br>
2017-11-16 10:49:37 -0200

    Resolution of Kata 1.

--- src/main/java/katas/Kata1.java
+++ src/main/java/katas/Kata1.java
@@ -1,22 +1,37 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import model.Movie;
-import util.DataUtil;
-
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.ImmutableMap;
+
+import model.Movie;
+import util.DataUtil;
 
 /*
     Goal: use map() to project an array of videos into an array of {id, title}-pairs
     DataSource: DataUtil.getMovies()
     Output: List of ImmutableMap.of("id", "5", "title", "Bad Boys")
 */
-public class Kata1 {
-    public static List<Map> execute() {
+public class Kata1
+{
+    @SuppressWarnings("rawtypes")
+    public static List<Map> execute()
+    {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
+        return movies.stream().map(t -> ImmutableMap.of("id", t.getId(), "title", t.getTitle())).collect(Collectors.toList());
+
+    }
+
+    @SuppressWarnings("rawtypes")
+    public static void main(String[] args)
+    {
+
+        List<Map> test = execute();
+
+        System.out.println(test);
+
     }
 }
