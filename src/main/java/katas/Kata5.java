df2cf0c11c315f53885422da774fee48ed6b5a02
André Machado Oliveira <amachado@fitec.org.br>
2017-11-16 14:21:01 -0200

    Resolution of Kata 5.

--- src/main/java/katas/Kata5.java
+++ src/main/java/katas/Kata5.java
@@ -1,24 +1,30 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
-import model.BoxArt;
-import model.Movie;
-import model.MovieList;
-import util.DataUtil;
-
 import java.util.List;
-import java.util.Map;
+
+import model.Movie;
+import util.DataUtil;
 
 /*
     Goal: Retrieve the largest rating using reduce()
     DataSource: DataUtil.getMovies()
     Output: Double
 */
-public class Kata5 {
-    public static Double execute() {
+public class Kata5
+{
+    public static Double execute()
+    {
         List<Movie> movies = DataUtil.getMovies();
 
-        return 3.0;
+        return movies.stream().reduce((movie1, movie2) -> movie1.getRating() > movie2.getRating() ? movie1 : movie2).map(movie -> movie.getRating()).get();
+
+    }
+
+    @SuppressWarnings("rawtypes")
+    public static void main(String[] args)
+    {
+
+        System.out.println(execute());
+
     }
 }
