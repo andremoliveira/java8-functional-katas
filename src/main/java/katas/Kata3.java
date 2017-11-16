73f2939b97f22e091933336cf4c706306faeb9fa
André Machado Oliveira <amachado@fitec.org.br>
2017-11-16 13:29:47 -0200

    Resolution of Kata 3.

--- src/main/java/katas/Kata3.java
+++ src/main/java/katas/Kata3.java
@@ -1,21 +1,28 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
-import model.Movie;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import model.MovieList;
 import util.DataUtil;
-
-import java.util.List;
 
 /*
     Goal: Use map() and flatMap() to project and flatten the movieLists into an array of video ids (flatMap(c -> c.stream()))
     DataSource: DataUtil.getMovieLists()
     Output: List of Integers
 */
-public class Kata3 {
-    public static List<Integer> execute() {
+public class Kata3
+{
+    public static List<Integer> execute()
+    {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(1, 2, 3);
+        return movieLists.stream().map(t -> t.getVideos()).flatMap(t -> t.stream()).map(t -> t.getId()).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(execute());
+
     }
 }
